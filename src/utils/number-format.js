ea01ebdcdaca2be0e6442d4167d51bf921dd005b
JustCharlz <charlie4biz@gmail.com>
2018-09-18 03:08:10 +0100

    removed src file

--- src/utils/number-format.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const formatAmount = (value) => {
-  if (!value || !value.toString) {
-    return value;
-  }
-  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-};
